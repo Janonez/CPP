48c91d6e38840c62ee2a0168e104d8b1d04e815a
Janonez <906269785@qq.com>
2023-03-26 00:58:42 +0800

    练习题

---
+++ 2_17_string模拟实现/2_17_string模拟实现/Test.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+//int main()
+//{
+//    string s("Let's take LeetCode contest");
+//    size_t pos = s.find(' ');
+//    // if(pos == string::npos)
+//    // {
+//    //     reverse(s.begin(),s.end());
+//    //     return 
+//    // }
+//    int i = 0;
+//    while (pos != string::npos)
+//    {
+//        reverse(s.begin() + i, s.begin() + pos);
+//        i = (pos + 1);
+//        pos = s.find(' ', pos + 1);
+//    }
+//    reverse(s.begin() + i, s.end());
+//
+//    cout << (int)'0';
+//    return 0;
+//}
+
+//int main()
+//{
+//    string num1("123"), num2("456");
+//    if (num1.size() < num2.size())
+//        num1.swap(num2);
+//    int s1 = 0;
+//    for (auto ch : num1)
+//    {
+//        s1 += (s1 * 10 + (ch - '0'));
+//    }
+//    int s2 = 0;
+//    int end2 = num2.size() - 1;
+//    //for(;end2>=0;--end2)
+//    for (auto ch : num2)
+//    {
+//        s2 += s2 * 10 + s1 * (ch - '0');
+//    }
+//    string s;
+//     s += s2;
+//
+//     cout << s2;
+//    return 0;
+//}
+
+int main()
+{
+    string num1("123");
+    string num2("456");
+    int len1 = num1.size();
+    int len2 = num2.size();
+    string res(len1 + len2, '0');
+    for (int i = len1 - 1; i >= 0; i--)
+    {
+        for (int j = len2 - 1; j >= 0; j--)
+        {
+            int tmp = (res[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0');
+            res[i + j + 1] = tmp % 10 + '0';
+            res[i + j] += tmp / 10;
+        }
+    }
+    for (int i = 0; i < len1 + len2; i++)
+    {
+        if (res[i] != '0')
+        {
+            cout << res.substr(i);
+            return 0;
+        }
+            
+    }
+    cout << "0";
+    return 0;
+}
