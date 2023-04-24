c064f88ae6d3020ddccfed5bd036d480726432e8
Janonez <906269785@qq.com>
2023-04-24 22:02:48 +0800

    继承

--- 3_23_继承/3_23_继承/Test.cpp
+++ 3_23_继承/3_23_继承/Test.cpp
@@ -102,74 +102,143 @@ using namespace std;
 //	return 0;
 //};
 
+//class Person
+//{
+//public:
+//	// 构造函数
+//	Person(const char* name = "peter")
+//		: _name(name)
+//	{
+//		cout << "Person()" << endl;
+//	}
+//	// 拷贝构造
+//	Person(const Person& p)
+//		: _name(p._name)
+//	{
+//		cout << "Person(const Person& p)" << endl;
+//	}
+//	Person& operator=(const Person & p)
+//	{
+//		cout << "Person operator=(const Person& p)" << endl;
+//		if (this != &p)
+//			_name = p._name;
+//		return *this;
+//	}
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//protected:
+//	string _name; // 姓名
+//};
+//
+//class Student : public Person
+//{
+//public:
+//	Student(const char* name, int num)
+//		: Person(name)
+//		, _num(num)
+//	{
+//		cout << "Student()" << endl;
+//	}
+//	Student(const Student& s)
+//		: Person(s)
+//		, _num(s._num)
+//	{
+//		cout << "Stduent(const Student& s)" << endl;
+//	}
+//	Student& operator=(const Student& s)
+//	{
+//		if (this != &s)
+//		{
+//			Person::operator=(s);
+//			_num = s._num;
+//		}
+//
+//		cout << "Student& operator=(const Student& s)" << endl;
+//
+//		return *this;
+//	}
+//protected:
+//	int _num;
+//};
+//
+//int main()
+//{
+//	Student s1("张三", 12);
+//	Student s2(s1);
+//
+//	Person p = s1;
+//
+//	s2 = s1;
+//	return 0;
+//}
+
+// 友元关系不能继承，也就是说基类友元不能访问子类私有和保护成员
+//class Student;
+//class Person
+//{
+//public:
+//	friend void Display(const Person& p, const Student& s);
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//public:
+//	friend void Display(const Person& p, const Student& s);
+//protected:
+//	int _stuNum; // 学号
+//};
+//void Display(const Person& p, const Student& s)
+//{
+//	cout << p._name << endl;
+//	cout << s._stuNum << endl;
+//}
+//int main()
+//{
+//	Person p;
+//	Student s;
+//	Display(p, s);
+//
+//	return 0;
+//}
+
+// 基类定义了static静态成员，则整个继承体系里面只有一个这样的成员。
+// 无论派生出多少个子类，都只有一个static成员实例。
 class Person
 {
 public:
-	// 构造函数
-	Person(const char* name = "peter")
-		: _name(name)
-	{
-		cout << "Person()" << endl;
-	}
-	// 拷贝构造
-	Person(const Person& p)
-		: _name(p._name)
-	{
-		cout << "Person(const Person& p)" << endl;
-	}
-	Person& operator=(const Person & p)
-	{
-		cout << "Person operator=(const Person& p)" << endl;
-		if (this != &p)
-			_name = p._name;
-		return *this;
-	}
-	~Person()
-	{
-		cout << "~Person()" << endl;
-	}
-protected:
+	Person() { ++_count; }
+//protected:
 	string _name; // 姓名
+public:
+	static int _count; // 统计人的个数。
 };
+int Person::_count = 0;
 
 class Student : public Person
 {
-public:
-	Student(const char* name, int num)
-		: Person(name)
-		, _num(num)
-	{
-		cout << "Student()" << endl;
-	}
-	Student(const Student& s)
-		: Person(s)
-		, _num(s._num)
-	{
-		cout << "Stduent(const Student& s)" << endl;
-	}
-	Student& operator=(const Student& s)
-	{
-		cout << "Student& operator=(const Student& s)" << endl;
-
-		if (this != &s)
-		{
-			Person::operator=(s);
-			_num = s._num;
-		}
-
-		return *this;
-	}
 protected:
-	int _num;
+	int _stuNum; // 学号
 };
-
+class Graduate : public Student {
+protected:
+	string _seminarCourse; // 研究科目
+};
 int main()
 {
-	Student s1("张三", 12);
-	Student s2(s1);
+	Person p;
+	Student s;
+	/*cout << &(p._name) << endl;
+	cout << &(s._name) << endl;
+	
+	cout << &(p._count) << endl;
+	cout << &(s._count) << endl;*/
 
-	Person p = s1;
+	Graduate g1;
+	Graduate g2;
 
-	s2 = s1;
+	cout << Person::_count << endl;
 	return 0;
 }
